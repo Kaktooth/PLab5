6a9a8bbbf5f0432dee78c34b65a9c91f14d1fe0d
Kaktooth <48326107+Kaktooth@users.noreply.github.com>
2021-05-12 22:27:07 +0300

    Add project files.

---
+++ Figure.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Text;
+using System.Windows.Forms;
+
+
+
+namespace PLab5
+{
+    
+public abstract class Figure
+    {
+       
+        public int x { get; set; }
+        public int y { get; set; }
+        public abstract void DrawBlack(PictureBox p);
+        public abstract void HideDrawingBackGround(PictureBox p);
+        public void MoveRight(PictureBox p)
+        { 
+            HideDrawingBackGround(p);
+            x+=10;
+            DrawBlack(p);
+            
+        }
+
+    }
+    class Circle : Figure
+    {
+        
+        double radius { get; set; }
+        public Circle() { }
+        public Circle(double radius) { this.radius = radius; }
+        public Circle(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public Circle(int x, int y, double radius)
+        {
+            this.radius = radius;
+            this.x = x;
+            this.y = y;
+        }
+        public override void DrawBlack(PictureBox p)
+        {
+
+            using (var g = Graphics.FromImage(p.Image))
+            {
+                g.FillEllipse(Brushes.Black, x, y, (int)radius, (int)radius);
+            }
+        }
+        public override void HideDrawingBackGround(PictureBox p)
+        {
+            using (var g = Graphics.FromImage(p.Image))
+            {
+                g.FillEllipse(Brushes.White, x, y, (int)radius, (int)radius);
+               
+            }
+           
+        }
+    }
+    class Square : Figure
+    {
+        private double side { get; set; }
+        public Square() { }
+        public Square(int sideLength) { this.side = sideLength; }
+        public Square(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public Square(int x, int y, double side)
+        {
+            this.side = side;
+            this.x = x;
+            this.y = y;
+        }
+        public override void DrawBlack(PictureBox p)
+        {
+            Pen c = Pens.Black;
+            using (var g = Graphics.FromImage(p.Image))
+            {
+                g.DrawRectangle(c,x, y, (int)side, (int)side);
+            }
+         
+        }
+        public override void HideDrawingBackGround(PictureBox p)
+        {
+            Pen c = Pens.White;
+            using (var g = Graphics.FromImage(p.Image))
+            {
+                g.DrawRectangle(c, x, y, (int)side, (int)side);
+            }
+          
+        }
+    }
+    class Rhomb : Figure
+    {
+        private double horDiag { get; set; }
+        
+        private double vertDiag { get; set; }
+    
+        public Rhomb() { }
+        public Rhomb(double horDiagLen, double vertDiagLen)
+        {
+            this.horDiag = horDiagLen;
+            this.vertDiag = vertDiagLen;
+        }
+        public Rhomb(int x, int y, double horDiagLen, double vertDiagLen)
+        {
+            this.x = x;
+            this.y = y;
+            this.horDiag = horDiagLen;
+            this.vertDiag = vertDiagLen;
+        }
+
+        public override void DrawBlack(PictureBox p)
+        {
+           
+            Pen c = Pens.Black;
+            using (var g = Graphics.FromImage(p.Image))
+            {
+
+                Rectangle r = new Rectangle(x, y, (int)horDiag, (int)vertDiag);
+                using (Matrix m = new Matrix())
+                {
+                    m.RotateAt(45, new PointF(r.Left + (r.Width / 2),
+                                              r.Top + (r.Height / 2)));
+
+                    g.Transform = m;
+                    g.DrawRectangle(c, r);
+
+                }
+
+              
+            }
+        }
+        public override void HideDrawingBackGround(PictureBox p)
+        {
+          
+            Pen c = Pens.White;
+            using (var g = Graphics.FromImage(p.Image))
+            {
+
+                Rectangle r = new Rectangle(x, y, (int)horDiag, (int)vertDiag);
+                using (Matrix m = new Matrix())
+                {
+                    m.RotateAt(45, new PointF(r.Left + (r.Width / 2),
+                                              r.Top + (r.Height / 2)));
+
+                    g.Transform = m;
+                    g.DrawRectangle(c, r);
+
+                }
+
+               
+            }
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PLab5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.Items.Add(FigureType.Circle);
+            comboBox1.Items.Add(FigureType.Square);
+            comboBox1.Items.Add(FigureType.Rhomb);
+            Bitmap b = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.Image = b;
+        }
+        public enum FigureType { Circle,Square,Rhomb}
+        FigureType figure { get; set; }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            figure = (FigureType)comboBox1.SelectedItem;
+            Figure f;
+            switch (figure)
+            {
+                case FigureType.Circle:
+                    double radius = Convert.ToDouble(textBox1.Text);
+                    f = new Circle(20, 200, radius);
+
+                    Task task = new Task(() =>
+                    {
+                        while (true)
+                        {
+                            if (f.x >=1000)
+                            {
+                                
+                                break;
+                            }
+                            f.MoveRight(pictureBox1);                          
+                            Thread.Sleep(100);
+                            this.Invoke(new MethodInvoker(delegate {
+                                pictureBox1.Refresh();
+                            }));
+                        }
+                    });
+                    task.Start();
+    
+
+                    break;
+                case FigureType.Square:
+                    double sideLength = Convert.ToDouble(textBox2.Text);
+                    f = new Square(20, 200, (int)sideLength);
+                    Task task2 = new Task(() =>
+                    {
+                        while (true)
+                        {
+                            if (f.x >= 1000)
+                            {
+
+                                break;
+                            }
+                            f.MoveRight(pictureBox1);
+                            Thread.Sleep(100);
+                            this.Invoke(new MethodInvoker(delegate {
+                                pictureBox1.Refresh();
+                            }));
+                        }
+                    });
+                    task2.Start();
+                    break;
+                case FigureType.Rhomb:
+                    double horizontal = Convert.ToDouble(textBox3.Text);
+                    double vertical = Convert.ToDouble(textBox4.Text);
+                    f = new Rhomb(20, 200, horizontal,vertical);
+                    Task task3 = new Task(() =>
+                    {
+                        while (true)
+                        {
+                            if (f.x >= 1000)
+                            {
+
+                                break;
+                            }
+                            f.MoveRight(pictureBox1);
+                            Thread.Sleep(100);
+                            this.Invoke(new MethodInvoker(delegate {
+                                pictureBox1.Refresh();
+                            }));
+                        }
+                    });
+                    task3.Start();
+                    break;
+            }
+        }
+    }
+}
